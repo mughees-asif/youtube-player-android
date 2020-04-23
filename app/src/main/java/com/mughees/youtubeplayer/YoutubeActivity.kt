edb92ef24000a3ec3b8087aa077ce4410d3d89ae
mughees-asif <mughees460@gmail.com>
2020-04-23 18:01:44 +0100

    youtube activity

--- app/src/main/java/com/mughees/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/mughees/youtubeplayer/YoutubeActivity.kt
@@ -2,6 +2,7 @@ package com.mughees.youtubeplayer
 
 import android.os.Bundle
 import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
@@ -9,5 +10,20 @@ class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
