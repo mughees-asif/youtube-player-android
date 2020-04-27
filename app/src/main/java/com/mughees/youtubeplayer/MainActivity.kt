a425daa9b8a7784aeab297949790253cc896e04e
mughees-asif <mughees460@gmail.com>
2020-04-27 22:39:21 +0100

    video activities

--- app/src/main/java/com/mughees/youtubeplayer/MainActivity.kt
+++ app/src/main/java/com/mughees/youtubeplayer/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.mughees.youtubeplayer
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPlaySingle.setOnClickListener(this)
+        btnStandAlone.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val intent = when (v.id) {
+            R.id.btnPlaySingle -> Intent(this, YoutubeActivity::class.java)
+            R.id.btnStandAlone -> Intent(this, YoutubeActivity::class.java)
+            else -> throw IllegalArgumentException("Undefined button clicked")
+        }
+        startActivity(intent)
     }
 }
